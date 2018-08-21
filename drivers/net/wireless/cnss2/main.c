1775ea20530700bb5cbacd98c4a1f52d92bb8a85
Kai Liu <kaliu@codeaurora.org>
2018-08-21 09:14:03 +0800

    cnss2: Clear flag CNSS_DRIVER_UNLOADING after unregister driver finished
    Flag CNSS_DRIVER_UNLOADING is set when unregister wlan started, but it's
    cleared in cnss_shutdown, which is also used in other call flow. Clear
    this flag after unregister finished.
    
    Change-Id: I0b5d63e5570c2604ff66d7852d7c7ca1c008a475
    Signed-off-by: Kai Liu <kaliu@codeaurora.org>

--- drivers/net/wireless/cnss2/main.c
+++ drivers/net/wireless/cnss2/main.c
@@ -1062,8 +1062,6 @@ static int cnss_qca6174_shutdown(struct cnss_plat_data *plat_priv)
 
 	cnss_power_off_device(plat_priv);
 
-	clear_bit(CNSS_DRIVER_UNLOADING, &plat_priv->driver_state);
-
 	return ret;
 }
 
@@ -1173,7 +1171,6 @@ static int cnss_qca6290_shutdown(struct cnss_plat_data *plat_priv)
 
 	clear_bit(CNSS_FW_READY, &plat_priv->driver_state);
 	clear_bit(CNSS_FW_MEM_READY, &plat_priv->driver_state);
-	clear_bit(CNSS_DRIVER_UNLOADING, &plat_priv->driver_state);
 
 	return ret;
 }
@@ -1672,6 +1669,7 @@ static int cnss_unregister_driver_hdlr(struct cnss_plat_data *plat_priv)
 	set_bit(CNSS_DRIVER_UNLOADING, &plat_priv->driver_state);
 	cnss_shutdown(plat_priv);
 	plat_priv->driver_ops = NULL;
+	clear_bit(CNSS_DRIVER_UNLOADING, &plat_priv->driver_state);
 
 	return 0;
 }
