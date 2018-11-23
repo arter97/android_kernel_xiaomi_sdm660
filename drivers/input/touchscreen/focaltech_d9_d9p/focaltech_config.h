8b34dfa94c437175e384efdf1e398c58e596e59f
Park Ju Hyung <qkrwngud825@gmail.com>
2018-11-24 07:36:22 +0900

    focaltech: optimize code
     - Disable unused FTS_REPORT_PRESSURE_EN feature
     - Include focaltech_i2c.c directly instead of linking it
       This allows better compiler optimizations
     - Remove unnecessary report_mutex
     - Remove various loggings
     - Remove unused key reporting feature
     - Mark inline for frequently used functions
     - Make use of likely() and unlikely()
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/input/touchscreen/focaltech_d9_d9p/focaltech_config.h
+++ drivers/input/touchscreen/focaltech_d9_d9p/focaltech_config.h
@@ -113,7 +113,7 @@
  * Report Pressure in multitouch
  * 1:enable(default),0:disable
 */
-#define FTS_REPORT_PRESSURE_EN                  1
+#define FTS_REPORT_PRESSURE_EN                  0
 
 /*
  * Gesture function enable
