fa525b8a63d1102dfe3ebd43ba77984a637e24e9
Park Ju Hyung <qkrwngud825@gmail.com>
2019-01-14 00:48:30 +0900

    build_kernel.sh: use 9.0.0 as the os version
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -53,7 +53,7 @@ mkbootimg \
     --ramdisk_offset 0x01000000 \
     --second_offset  0x00f00000 \
     --tags_offset    0x00000100 \
-    --os_version     8.1.0 \
+    --os_version     9.0.0 \
     --os_patch_level 2018-12 \
     --header_version 0 \
     -o $KERNELDIR/boot.img
