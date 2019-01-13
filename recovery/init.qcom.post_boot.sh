d2f2255b47a873f595c4fdfbcf1598620b033f02
Park Ju Hyung <qkrwngud825@gmail.com>
2019-01-14 01:31:36 +0900

    Add custom TWRP and ready for release
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

---
+++ recovery/init.qcom.post_boot.sh
@@ -0,0 +1,211 @@
+#!/sbin/sh
+
+# Copyright (c) 2012-2013, 2016-2018, The Linux Foundation. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of The Linux Foundation nor
+#       the names of its contributors may be used to endorse or promote
+#       products derived from this software without specific prior written
+#       permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# disable thermal bcl hotplug to switch governor
+echo 0 > /sys/module/msm_thermal/core_control/enabled
+
+# Bring all cores online
+echo 1 > /sys/devices/system/cpu/cpu0/online
+echo 1 > /sys/devices/system/cpu/cpu1/online
+echo 1 > /sys/devices/system/cpu/cpu2/online
+echo 1 > /sys/devices/system/cpu/cpu3/online
+echo 1 > /sys/devices/system/cpu/cpu4/online
+echo 1 > /sys/devices/system/cpu/cpu5/online
+echo 1 > /sys/devices/system/cpu/cpu6/online
+echo 1 > /sys/devices/system/cpu/cpu7/online
+
+# Setup readahead
+find /sys/devices -name read_ahead_kb | while read node; do echo 256 > $node; done
+
+# Set the default IRQ affinity to the primary cluster. When a
+# CPU is isolated/hotplugged, the IRQ affinity is adjusted
+# to one of the CPU from the default IRQ affinity mask.
+echo f > /proc/irq/default_smp_affinity
+
+echo 2 > /proc/sys/kernel/sched_window_stats_policy
+echo 5 > /proc/sys/kernel/sched_ravg_hist_size
+
+echo 2 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
+echo 60 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
+echo 30 > /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres
+echo 100 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
+echo 1 > /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster
+echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
+
+# Setting b.L scheduler parameters
+echo 96 > /proc/sys/kernel/sched_upmigrate
+echo 90 > /proc/sys/kernel/sched_downmigrate
+echo 140 > /proc/sys/kernel/sched_group_upmigrate
+echo 120 > /proc/sys/kernel/sched_group_downmigrate
+echo 0 > /proc/sys/kernel/sched_select_prev_cpu_us
+echo 400000 > /proc/sys/kernel/sched_freq_inc_notify
+echo 400000 > /proc/sys/kernel/sched_freq_dec_notify
+echo 5 > /proc/sys/kernel/sched_spill_nr_run
+echo 1 > /proc/sys/kernel/sched_restrict_cluster_spill
+echo 100000 > /proc/sys/kernel/sched_short_burst_ns
+echo 1 > /proc/sys/kernel/sched_prefer_sync_wakee_to_waker
+echo 20 > /proc/sys/kernel/sched_small_wakee_task_load
+
+# cpuset settings
+echo 0-3 > /dev/cpuset/background/cpus
+echo 0-3 > /dev/cpuset/system-background/cpus
+
+# configure governor settings for little cluster
+echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load
+echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif
+echo "19000 1401600:39000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay
+echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load
+echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate
+echo 1401600 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq
+echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy
+echo "85 1747200:95" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
+echo 39000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time
+echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis
+echo 633600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/ignore_hispeed_on_notif
+echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/fast_ramp_down
+
+# configure governor settings for big cluster
+echo "interactive" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load
+echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif
+echo "19000 1401600:39000" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay
+echo 90 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
+echo 20000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate
+echo 1401600 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq
+echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy
+echo "85 1401600:90 2150400:95" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads
+echo 39000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time
+echo 59000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis
+echo 1113600 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/ignore_hispeed_on_notif
+echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/fast_ramp_down
+
+# configure LPM
+echo N > /sys/module/lpm_levels/system/pwr/cpu0/ret/idle_enabled
+echo N > /sys/module/lpm_levels/system/pwr/cpu1/ret/idle_enabled
+echo N > /sys/module/lpm_levels/system/pwr/cpu2/ret/idle_enabled
+echo N > /sys/module/lpm_levels/system/pwr/cpu3/ret/idle_enabled
+echo N > /sys/module/lpm_levels/system/perf/cpu4/ret/idle_enabled
+echo N > /sys/module/lpm_levels/system/perf/cpu5/ret/idle_enabled
+echo N > /sys/module/lpm_levels/system/perf/cpu6/ret/idle_enabled
+echo N > /sys/module/lpm_levels/system/perf/cpu7/ret/idle_enabled
+echo N > /sys/module/lpm_levels/system/pwr/pwr-l2-dynret/idle_enabled
+echo N > /sys/module/lpm_levels/system/perf/perf-l2-dynret/idle_enabled
+echo N > /sys/module/lpm_levels/system/pwr/pwr-l2-ret/idle_enabled
+echo N > /sys/module/lpm_levels/system/perf/perf-l2-ret/idle_enabled
+# enable LPM
+echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
+
+# Enable bus-dcvs
+for cpubw in /sys/class/devfreq/*qcom,cpubw*
+do
+    echo "bw_hwmon" > $cpubw/governor
+    echo 50 > $cpubw/polling_interval
+    echo 762 > $cpubw/min_freq
+    echo "1525 3143 5859 7759 9887 10327 11863 13763" > $cpubw/bw_hwmon/mbps_zones
+    echo 4 > $cpubw/bw_hwmon/sample_ms
+    echo 85 > $cpubw/bw_hwmon/io_percent
+    echo 100 > $cpubw/bw_hwmon/decay_rate
+    echo 50 > $cpubw/bw_hwmon/bw_step
+    echo 20 > $cpubw/bw_hwmon/hist_memory
+    echo 0 > $cpubw/bw_hwmon/hyst_length
+    echo 80 > $cpubw/bw_hwmon/down_thres
+    echo 0 > $cpubw/bw_hwmon/low_power_ceil_mbps
+    echo 34 > $cpubw/bw_hwmon/low_power_io_percent
+    echo 20 > $cpubw/bw_hwmon/low_power_delay
+    echo 0 > $cpubw/bw_hwmon/guard_band_mbps
+    echo 250 > $cpubw/bw_hwmon/up_scale
+    echo 1600 > $cpubw/bw_hwmon/idle_mbps
+done
+
+for memlat in /sys/class/devfreq/*qcom,memlat-cpu*
+do
+    echo "mem_latency" > $memlat/governor
+    echo 10 > $memlat/polling_interval
+    echo 400 > $memlat/mem_latency/ratio_ceil
+done
+echo "cpufreq" > /sys/class/devfreq/soc:qcom,mincpubw/governor
+
+# Start cdsprpcd only for sdm660 and disable for sdm630
+start vendor.cdsprpcd
+
+# Let kernel know our image version/variant/crm_version
+if [ -f /sys/devices/soc0/select_image ]; then
+    image_version="10:"
+    image_version+=`getprop ro.build.id`
+    image_version+=":"
+    image_version+=`getprop ro.build.version.incremental`
+    image_variant=`getprop ro.product.name`
+    image_variant+="-"
+    image_variant+=`getprop ro.build.type`
+    oem_version=`getprop ro.build.version.codename`
+    echo 10 > /sys/devices/soc0/select_image
+    echo $image_version > /sys/devices/soc0/image_version
+    echo $image_variant > /sys/devices/soc0/image_variant
+    echo $oem_version > /sys/devices/soc0/image_crm_version
+fi
+
+# Configure input boost
+echo "1248000 1344000" > /sys/module/cpu_boost/parameters/input_boost_freq
+echo 90 > /sys/module/cpu_boost/parameters/input_boost_ms
+echo "1171200 1190400" > /sys/module/cpu_boost/parameters/input_boost_freq_s2
+echo 1000 > /sys/module/cpu_boost/parameters/input_boost_ms_s2
+
+# Configure CPU governor
+echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/enable_prediction
+echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/enable_prediction
+echo 59000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis
+
+# Set min freq for CPUs
+echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 300000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+
+# Disable console suspend
+echo N > /sys/module/printk/parameters/console_suspend
+
+# Disable userspace intervention
+chmod 444 \
+  /sys/module/cpu_boost/parameters/input_boost_freq \
+  /sys/module/cpu_boost/parameters/input_boost_ms \
+  /sys/module/cpu_boost/parameters/input_boost_freq_s2 \
+  /sys/module/cpu_boost/parameters/input_boost_ms_s2 \
+  /sys/devices/system/cpu/cpu0/cpufreq/interactive/enable_prediction \
+  /sys/devices/system/cpu/cpu4/cpufreq/interactive/enable_prediction \
+  /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis \
+  /sys/module/printk/parameters/console_suspend
+
+# re-enable thermal and BCL hotplug
+echo 1 > /sys/module/msm_thermal/core_control/enabled
+
+# Parse misc partition path and set property
+misc_link=$(ls -l /dev/block/bootdevice/by-name/misc)
+real_path=${misc_link##*>}
+setprop persist.vendor.mmi.misc_dev_path $real_path
